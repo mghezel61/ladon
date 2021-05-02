20d962d04c7c9619956839c4060cf128b321f7bf
m.ghezel <ghezel1361@yahoo.com>
2021-05-02 17:46:25 +0200

    Create the components of the website

---
+++ src/component/Main.js
@@ -0,0 +1,14 @@
+import HotelInfo from "./HotelInfo";
+import React from "react";
+import Welcome from "./Welcome";
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+    <Welcome />
+    <HotelInfo />
+    </main>
+  );
+};
+
+export default Main;
